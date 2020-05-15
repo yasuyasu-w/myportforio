cb5185ef5c59612db13079fb3531a41ec4908cad
yasuyasu-w <ya.su.ya.su.w0612@gmail.com>
2020-05-15 09:32:28 +0000

    react-route-domをインストール

--- src/Bar/MiniDrawer.tsx
+++ src/Bar/MiniDrawer.tsx
@@ -22,6 +22,8 @@ import FitnessCenterIcon from '@material-ui/icons/FitnessCenter';
 import LaptopChromebookIcon from '@material-ui/icons/LaptopChromebook';
 import MailIcon from '@material-ui/icons/Mail';
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -123,7 +125,7 @@ export default function MiniDrawer() {
          <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap>
-            ポートフォリオ
+            渡邉靖紋のポートフォリオ
           </Typography>
         </Toolbar>
       </AppBar>
@@ -192,24 +194,6 @@ export default function MiniDrawer() {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
           ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
           facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
         </Typography>
       </main>
     </div>
