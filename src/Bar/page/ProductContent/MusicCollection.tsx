0eb4f92c4927b9504059fe66513856e9e3b81bfa
yasuyasu-w <ya.su.ya.su.w0612@gmail.com>
2020-06-17 04:40:34 +0000

    MusicCollection.tsxの中身のリンク、ソースコード、概要を追加した

--- src/Bar/page/ProductContent/MusicCollection.tsx
+++ src/Bar/page/ProductContent/MusicCollection.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardMedia from '@material-ui/core/CardMedia';
 import musicCollectImage from '../img/music-collec-main.png'
 
@@ -9,7 +10,15 @@ const useStyles=makeStyles({
         width:'100%'
     },
     cardMedia:{
-        height:'100%'
+        height:'500px'
+    },
+    sentence:{
+        marginTop:'5%',
+        marginBottom:'5%'
+    },
+    link_source:{
+        display:'flex',
+        justifyContent:'space-around'
     }
 })
 
@@ -18,12 +27,33 @@ const MusicContent=()=>{
 
     return (
         <>
-        <div>music</div>
         <Card className={classes.rootCard}>
+           <CardActionArea>
             <CardMedia className={classes.cardMedia}
                        title='Contemplative Reptile'
-                       image={musicCollectImage} />
+                       image={musicCollectImage} />           
+          </CardActionArea>  
         </Card>
+        <div>
+            <div className={classes.link_source+" "+classes.sentence}>
+            <div>
+                <h3>リンク</h3>
+                <a href="https://music-collection-ys.firebaseapp.com/"
+                   target="_blank"
+                   rel="noopener noreferrer">music collection</a>
+            </div>
+            <div>
+                <h3>ソースコード</h3>
+                <a href="https://github.com/yasuyasu-w/music-collection-1/"
+                   target="_blank"
+                   rel="noopener noreferrer">githubへ</a>
+            </div>
+            </div>
+            <div className={classes.sentence}>
+            <h3>概要</h3>
+            <p>お気に入りの音楽のメモツール</p>
+            </div>
+        </div>
         </>
     )
 }
